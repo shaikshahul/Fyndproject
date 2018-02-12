45bfb394934e21ba51d101c136ecdcc75c39e711
Python Developer <dev@brio.co.in>
2018-02-12 15:40:26 +0530

    updating to python 3, requirements

--- FyndApp/Views/addmovies.py
+++ FyndApp/Views/addmovies.py
@@ -47,7 +47,7 @@ def movies(request):
         movobj.country = country
         movobj.youtube = youtube_url
         movobj.storyline = storyline
-        movobj.release_date = datetime.strptime(
+        movobj.release_date = datetime.datetime.strptime(
                 releasedate, "%Y-%m-%d").date()
         movobj.save()
         return redirect('/movies/')
