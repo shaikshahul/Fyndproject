4025c53d315d6ac2d0c4fe64d10e22fad6b779ec
Python Developer <dev@brio.co.in>
2018-02-12 13:58:20 +0530

    updating to python 3, requirements

--- FyndApp/Views/serializers.py
+++ FyndApp/Views/serializers.py
@@ -1,12 +1,1 @@
-from rest_framework import serializers
-from FyndApp.models import Movies
-
-
-class MoviesSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Movies
-        fields = ('name','director','genre','score','popularity')
-    
-
     
--- FyndProject/settings.py
+++ FyndProject/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'FyndApp',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
