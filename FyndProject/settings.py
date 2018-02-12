bd4a198b5dd08e195fc9e798b8d511a65c141c87
Python Developer <dev@brio.co.in>
2018-02-12 12:41:25 +0530

    ready for deply

--- FyndProject/settings.py
+++ FyndProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^lzpvo=h7vj#o(!ff=w8mehlcg3^kb!*o&g44i0-2z)j8mq3m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
